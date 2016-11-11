261441417f8304ce720c7ded2bf478b7e8559c8a
ryoff <ryoffes@gmail.com>
2016-11-12 00:54:19 +0900

    Add .excise_tax_rate

--- lib/excise_tax_jp.rb
+++ lib/excise_tax_jp.rb
@@ -1,7 +1,24 @@
+require "bigdecimal"
+require 'active_support'
 require "excise_tax_jp/version"
+require "excise_tax_jp/core_ext/integer"
 
 module ExciseTaxJp
   RATE103 = BigDecimal("1.03").freeze
   RATE105 = BigDecimal("1.05").freeze
   RATE108 = BigDecimal("1.08").freeze
+
+  class << self
+    def excise_tax_rate(date: Date.current)
+      if (Date.new(1989, 4, 1)..Date.new(1997, 3, 31)).cover?(date)
+        ExciseTaxJp::RATE103
+      elsif (Date.new(1997, 4, 1)..Date.new(2014, 3, 31)).cover?(date)
+        ExciseTaxJp::RATE105
+      elsif (Date.new(2014, 4, 1)..Date::Infinity.new).cover?(date)
+        ExciseTaxJp::RATE108
+      else
+        0
+      end
+    end
+  end
 end
