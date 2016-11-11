574dae094fb777ff13a3ca3e03106725dc5c3994
ryoff <ryoffes@gmail.com>
2016-11-12 00:54:35 +0900

    Use pry

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'rubygems'
+require 'pry'
 require 'excise_tax_jp'
 require 'active_support/core_ext/date'
 require 'rspec-parameterized'
