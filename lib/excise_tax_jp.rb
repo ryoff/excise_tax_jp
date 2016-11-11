86e3538c6bf3c4b306696576e660d4c5fa45afe1
ryoff <ryoffes@gmail.com>
2016-11-11 22:19:24 +0900

    Add const

--- lib/excise_tax_jp.rb
+++ lib/excise_tax_jp.rb
@@ -1,5 +1,7 @@
 require "excise_tax_jp/version"
 
 module ExciseTaxJp
-  # Your code goes here...
+  RATE103 = BigDecimal("1.03").freeze
+  RATE105 = BigDecimal("1.05").freeze
+  RATE108 = BigDecimal("1.08").freeze
 end
