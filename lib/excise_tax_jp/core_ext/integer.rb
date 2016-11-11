67a8eddd34818a19bf5ee6a6501e7cf8a64310eb
ryoff <ryoffes@gmail.com>
2016-11-12 00:53:54 +0900

    Add lib/excise_tax_jp/core_ext/integer.rb

---
+++ lib/excise_tax_jp/core_ext/integer.rb
@@ -0,0 +1,11 @@
+class Integer
+  def with_excise_tax(date: Date.current, fraction: :floor)
+    return self if self < 0
+
+    (BigDecimal(self) * ExciseTaxJp.excise_tax_rate(date: date)).__send__(fraction)
+  end
+
+  def excise_tax(date: Date.current, fraction: :floor)
+    with_excise_tax(date: date, fraction: fraction) - self
+  end
+end
