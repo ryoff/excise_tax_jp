a77510e7578b9a3655901431bae47d82f0a38578
ryoff <ryoffes@gmail.com>
2016-11-12 23:59:08 +0900

    Add test case

--- spec/excise_tax_jp_spec.rb
+++ spec/excise_tax_jp_spec.rb
@@ -28,7 +28,19 @@ describe ExciseTaxJp do
     describe "#with_excise_tax" do
       where(:integer, :date, :fraction, :amount) do
         [
-          [1000, Date.new(2016, 1, 1), nil, 1080]
+          [10,   Date.new(2016, 1, 1), nil, 10],
+          [20,   Date.new(2016, 1, 1), nil, 21],
+          [30,   Date.new(2016, 1, 1), nil, 32],
+          [40,   Date.new(2016, 1, 1), nil, 43],
+          [50,   Date.new(2016, 1, 1), nil, 54],
+          [60,   Date.new(2016, 1, 1), nil, 64],
+          [70,   Date.new(2016, 1, 1), nil, 75],
+          [80,   Date.new(2016, 1, 1), nil, 86],
+          [90,   Date.new(2016, 1, 1), nil, 97],
+          [100,  Date.new(2016, 1, 1), nil, 108],
+          [198,  Date.new(2016, 1, 1), nil, 213],
+          [0,    Date.new(2016, 1, 1), nil, 0],
+          [-100, Date.new(2016, 1, 1), nil, -100],
         ]
       end
 
